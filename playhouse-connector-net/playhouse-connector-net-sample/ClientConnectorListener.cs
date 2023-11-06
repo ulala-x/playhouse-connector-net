38826b08af0ef0e834b69372025e3a16e1669e0a
ulala-x <ulala.the.great@gmail.com>
2023-11-06 18:05:26 +0900

    주석처리

--- playhouse-connector-net/playhouse-connector-net-sample/ClientConnectorListener.cs
+++ playhouse-connector-net/playhouse-connector-net-sample/ClientConnectorListener.cs
@@ -1,28 +1,28 @@
-﻿using playhouse_connector_net.network;
-using PlayHouseConnector;
-using PlayHouseConnector.network;
-using System.Collections.Concurrent;
-
-namespace playhouse_connector_net_sample
-{
-    class ClientConnectorListener : IConnectorListener
-    {
-        private ConcurrentQueue<Packet> _packetQueue = new ConcurrentQueue<Packet>();
-
-
-        public void OnConnected()
-        {
-            LOG.Info($"onConnected",GetType());
-        }
-
-        public void OnDisconnected()
-        {
-            LOG.Info($"OnDisconnected", GetType());
-        }
-
-        public void OnReceive(ClientPacket clientPacket)
-        {
-            _packetQueue.Enqueue(clientPacket.ToPacket());
-        }
-    }
-}
+﻿// using playhouse_connector_net.network;
+// using PlayHouseConnector;
+// using PlayHouseConnector.network;
+// using System.Collections.Concurrent;
+//
+// namespace playhouse_connector_net_sample
+// {
+//     class ClientConnectorListener : IConnectorListener
+//     {
+//         private ConcurrentQueue<Packet> _packetQueue = new ConcurrentQueue<Packet>();
+//
+//
+//         public void OnConnected()
+//         {
+//             LOG.Info($"onConnected",GetType());
+//         }
+//
+//         public void OnDisconnected()
+//         {
+//             LOG.Info($"OnDisconnected", GetType());
+//         }
+//
+//         public void OnReceive(ClientPacket clientPacket)
+//         {
+//             _packetQueue.Enqueue(clientPacket.ToPacket());
+//         }
+//     }
+// }
--- playhouse-connector-net/playhouse-connector-net-sample/Program.cs
+++ playhouse-connector-net/playhouse-connector-net-sample/Program.cs
@@ -1,68 +1,70 @@
-﻿using Playhouse.Sample;
-using playhouse_connector_net;
-using PlayHouseConnector;
-
-namespace playhouse_connector_net_sample
-{
-    internal class Program
-    {
-
-        static void Main(string[] args)
-        {
-
-            ConnectorConfig config = new ConnectorConfig();
-            ClientConnectorListener listener = new ClientConnectorListener();
-
-            Connector connector = new Connector(config);
-
-            connector.Start();
-
-            connector.Connect("127.0.0.1", 10001);
-            short serviceId = 2;
-
-            var authenticateReq = new AuthenticateReq();
-            authenticateReq.UserId= 1;
-            authenticateReq.Token= "test";
-
-            connector.OnConnect += () =>
-            {
-                Console.WriteLine("OnConnected");
-            };
-
-            connector.OnDiconnect += () =>
-            {
-                Console.WriteLine("OnDisconnected");
-            };
-
-            connector.OnReconnect += (int retryCnt) =>
-            {
-                Console.WriteLine($"OnConnected {retryCnt}");
-            };
-
-            connector.OnApiReceive += (short serviceId, Packet packet) =>
-            {
-                Console.WriteLine($"OnReceive {serviceId},{packet.MsgId}");
-            };
-
-            //connector.OnApiReceive()
-            //connector.OnStageReceive(short serviceId,int stageIndex,Packet packet)
-
-
-
-
-            connector.RequestToApi(serviceId, new Packet(authenticateReq), (IReplyPacket replyPacket) =>
-            {
-                using (replyPacket)
-                {
-                    if (replyPacket.ErrorCode == 0)
-                    {
-                        AuthenticateRes authenticateRes = AuthenticateRes.Parser.ParseFrom(replyPacket.Data);
-                    }
-                }   
-            });
-            
-
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// using Playhouse.Sample;
+// using playhouse_connector_net;
+// using PlayHouseConnector;
+//
+// namespace playhouse_connector_net_sample
+// {
+//     internal class Program
+//     {
+//
+//         static void Main(string[] args)
+//         {
+//
+//             ConnectorConfig config = new ConnectorConfig();
+//             ClientConnectorListener listener = new ClientConnectorListener();
+//
+//             Connector connector = new Connector(config);
+//
+//             connector.Start();
+//
+//             connector.Connect("127.0.0.1", 10001);
+//             short serviceId = 2;
+//
+//             var authenticateReq = new AuthenticateReq();
+//             authenticateReq.UserId= 1;
+//             authenticateReq.Token= "test";
+//
+//             connector.OnConnect += () =>
+//             {
+//                 Console.WriteLine("OnConnected");
+//             };
+//
+//             connector.OnDiconnect += () =>
+//             {
+//                 Console.WriteLine("OnDisconnected");
+//             };
+//
+//             connector.OnReconnect += (int retryCnt) =>
+//             {
+//                 Console.WriteLine($"OnConnected {retryCnt}");
+//             };
+//
+//             connector.OnApiReceive += (short serviceId, Packet packet) =>
+//             {
+//                 Console.WriteLine($"OnReceive {serviceId},{packet.MsgId}");
+//             };
+//
+//             //connector.OnApiReceive()
+//             //connector.OnStageReceive(short serviceId,int stageIndex,Packet packet)
+//
+//
+//
+//
+//             connector.RequestToApi(serviceId, new Packet(authenticateReq), (IReplyPacket replyPacket) =>
+//             {
+//                 using (replyPacket)
+//                 {
+//                     if (replyPacket.ErrorCode == 0)
+//                     {
+//                         AuthenticateRes authenticateRes = AuthenticateRes.Parser.ParseFrom(replyPacket.Data);
+//                     }
+//                 }   
+//             });
+//             
+//
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
+static void Main(string[] args)
+{}
