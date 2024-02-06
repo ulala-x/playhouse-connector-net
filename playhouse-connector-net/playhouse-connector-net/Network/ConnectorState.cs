035dfa4289c7dab85e9807404167119b87c072d0
ulala-x <ulala.the.great@gmail.com>
2024-02-06 10:07:19 +0900

    추가 파일

---
+++ playhouse-connector-net/playhouse-connector-net/Network/ConnectorState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayHouseConnector.Network
+{
+    public enum ConnectorState
+    {
+        Before_Connected = 0,
+        Before_Authenticated = 1,
+        Ready = 2,
+        Disconnected = 3
+    }
+
+    public enum DisconnectType 
+    { 
+        None = 0,
+        System_Disconnect = 1,
+        Self_Disconnect = 2,
+        AuthenticateFail_Disconnect = 3
+    }
+
+}
