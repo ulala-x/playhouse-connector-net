544e748ce75cbd19cd8a97e196b0e7034dca8425
ulala-x <ulala.the.great@gmail.com>
2024-11-07 05:13:47 +0900

    fix - parser

--- playhouse-connector-net/playhouse-connector-net/network/PacketParser.cs
+++ playhouse-connector-net/playhouse-connector-net/network/PacketParser.cs
@@ -40,7 +40,7 @@ namespace PlayHouseConnector.Network
                     throw new IndexOutOfRangeException("BodySizeOver");
                 }
 
-                int checkSizeOfMsg = buffer.PeekByte(buffer.MoveIndex(buffer.ReaderIndex, +4 + 2));
+                int checkSizeOfMsg = buffer.PeekByte(buffer.MoveIndex(buffer.ReaderIndex, 4 + 2));
 
                 // If the remaining buffer is smaller than the expected packet size, wait for more data
                 if (buffer.Count < bodySize + checkSizeOfMsg + PacketConst.MinPacketSize)
