9843f9a5fc909e47fe224453254a62a06ac85ce1
ulala-x <ulala.the.great@gmail.com>
2024-11-07 00:55:08 +0900

    fix : config 설정 수정

--- playhouse-connector-net/playhouse-connector-net/ConnectorConfig.cs
+++ playhouse-connector-net/playhouse-connector-net/ConnectorConfig.cs
@@ -5,17 +5,17 @@
         /// <summary>
         ///     서버와의 연결을 확인하기 위한 connection idle timeout 설정(단위 : mili sec, 0일 경우 사용 안함, 기본값 30000).
         /// </summary>
-        public int ConnectionIdleTimeoutMs = 30000;
+        public int ConnectionIdleTimeoutMs { get; set; } = 30000;
 
         /// <summary>
         ///     서버와의 연결을 확인하기 위한 heartbeat 주기 설정(단위 : mili sec, 0일 경우 사용 안함, 기본값 1000).
         /// </summary>
-        public int HeartBeatIntervalMs = 1000;
+        public int HeartBeatIntervalMs { get; set; } = 10000;
 
         /// <summary>
         ///     TimeOut 기본 대기시간 설정(단위 : ms, 기본값 6000).
         /// </summary>
-        public int RequestTimeoutMs = 6000;
+        public int RequestTimeoutMs { get; set; } = 30000;
 
         public bool UseWebsocket { get; set; } = false;
 
